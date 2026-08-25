b2558c0643ecbd00a11156d507e29956368d01d7
mezzdev <mezzmonteur@gmail.com>
2026-08-25 21:10:11 +0200

    Remove obsolete vehicle 3D script

--- js/vehicle.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  'use strict';
-  /* Vehicle mode removed by design: Hiro uses a pure procedural 3D environment.
-     No external 3D model, no duplicate Three.js instance, no forced scrolling. */
-  document.documentElement.classList.add('hiro-vehicle-disabled');
-})();
