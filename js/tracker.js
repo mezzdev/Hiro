4688103f1081d76dccdfe61ed6fc8cc419ba2516
mezzdev <mezzmonteur@gmail.com>
2026-08-25 21:53:12 +0200

    Remove tracker comments

--- js/tracker.js
+++ js/tracker.js
@@ -1,5 +1,3 @@
 (() => {
-    'use strict';
-    // GitHub Pages has no /api/visit backend. Keep this file as a harmless
-    // compatibility stub so older deployments never generate a 404.
+    "use strict";
 })();
