02899343db5a92aa015254e3e9759f4f9ea5f981
mezzdev <mezzmonteur@gmail.com>
2026-08-25 21:10:08 +0200

    Remove obsolete 3D compatibility script

--- js/experience.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  'use strict';
-  /* Compatibility layer: the 3D engine now lives in main.js.
-     This file intentionally contains no section transitions and no models. */
-  document.documentElement.classList.add('hiro-experience-ready');
-})();
